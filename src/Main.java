79ae5731209813908fd40e489c6d0cd30aca2b57
guswns1659 <guswns1659@gmail.com>
2020-02-18 19:25:03 +0900

    20.02.18) 스프링부트 step1 반복 및 heroku 사용법 숙지
    어제보다 추가적인 기능 구현은 없다. 처음부터 1번 반복.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,6 @@
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
-        awtTest();
+        System.out.println("hello world");
     }
 
-    private static void awtTest() {
-        Frame frame = new Frame("Login");
-        frame.setSize(300, 200);
-        frame.setLayout(null);
-
-        Button button = new Button("check");
-        button.setSize(100, 30);
-        button.setLocation(100, 50);
-
-        Panel panel = new Panel();
-
-
-        frame.add(panel);
-        frame.add(button);
-        frame.setVisible(true);
-
-    }
 }
